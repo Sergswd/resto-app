6dc1c30ee80650a8b2045271bcf68b8317550bb1
serghv <falko.serega2016@yandex.ru>
2021-12-27 23:11:49 +0300

    deploy app

---
+++ build/precache-manifest.0d02f1985ea2bd9d8ef1487a80cc8d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394861a8d090a2fac32d0afb6872b0f0",
+    "url": "/resto-app/index.html"
+  },
+  {
+    "revision": "01ab5d26d21d58abd6fb",
+    "url": "/resto-app/static/css/main.f52ad10e.chunk.css"
+  },
+  {
+    "revision": "973e9999c425b04258d9",
+    "url": "/resto-app/static/js/2.f65e8f25.chunk.js"
+  },
+  {
+    "revision": "01ab5d26d21d58abd6fb",
+    "url": "/resto-app/static/js/main.ad568412.chunk.js"
+  },
+  {
+    "revision": "3e0d861e4ffa6af955d6",
+    "url": "/resto-app/static/js/runtime~main.2514cf48.js"
+  },
+  {
+    "revision": "2b22f4b818579f3da4efcea26c8770e6",
+    "url": "/resto-app/static/media/bg2.2b22f4b8.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/resto-app/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
